ae83919b36ed7df7ba8480ca32b1c6ec5797c01d
udit <yudistrange@gmail.com>
2020-05-15 14:40:43 +0530

    Add an image loader component which has error handling

---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <img v-bind:src="this.source" v-on:error="this.onImageLoadError">
+</template>
+
+<script>
+export default {
+  name: 'ImageContainer',
+  props: {
+    source: ''
+  },
+  methods: {
+    onImageLoadError () {
+      // Set the images as invalid
+      this.$store.commit('invalidImages')
+    }
+  },
+  created: function () {
+    console.log('created Image container', this.leftImageSrc)
+  },
+  updated: function () {
+    console.log('updated Image container', this.leftImageSrc)
+  }
+}
+</script>
